0e60de4438d28a085c9f1cc9a21f3181834f351b
Elise Erickson <elisece717@gmail.com>
2024-01-03 11:08:28 -0500

    adds directors page and directorcards

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,14 +1,27 @@
 import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import DirectorCard from "../components/DirectorCard";
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+
+  useEffect( () => {
+    fetch("http://localhost:4000/directors")
+    .then(res => res.json())
+    .then(data => setDirectors(data))
+    }, []
+  )
+
   return (
     <>
       <header>
-      <NavBar/>
+        <NavBar/>
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+          {
+            directors.map(dir => <DirectorCard name={dir.name} movies={dir.movies} key={dir.id}/>)
+          }
       </main>
     </>
   );

9fe5ab8c1bacf13cfaf1e575d063d834909016a1
Elise Erickson <elisece717@gmail.com>
2024-01-03 11:25:24 -0500

    adds and populates movie page

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,14 +1,29 @@
 import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
 
 function Movie() {
+  const id = useParams().id
+  const [movie, setMovie] = useState({})
+
+  useEffect( ()=> {
+    fetch(`http://localhost:4000/movies/${id}`)
+    .then(res => res.json())
+    .then(data => setMovie(data))
+  }, [id])
+
+
   return (
     <>
       <header>
-      <NavBar/>
+        <NavBar/>
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {
+          movie.genres.map( genre => <span key={genre}>{genre}</span>)
+        }
       </main>
     </>
   );

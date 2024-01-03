9e92ad87bfa02a4976e69752d1f500603891c877
Elise Erickson <elisece717@gmail.com>
2024-01-03 11:16:21 -0500

    adds actors from database

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import ActorCard from "../components/ActorCard";
 
 function Actors() {
+
+  const [actors, setActors] = useState([])
+
+  useEffect( () => {
+    fetch("http://localhost:4000/actors")
+    .then(res => res.json())
+    .then(data => setActors(data))
+  }, [])
+
   return (
     <>
       <header>
         <NavBar/>
       </header>
       <main>
-        {/* Actor info here! */}
+        {actors.map( actor => <ActorCard name={actor.name} movies={actor.movies} key={actor.id}/>)}
       </main>
     </>
   );

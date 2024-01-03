310a75d5ea09f8c9d41c8eaf5fa2a5cc6d4c10cf
Elise Erickson <elisece717@gmail.com>
2024-01-03 11:28:50 -0500

    adds errorpage

--- src/routes.js
+++ src/routes.js
@@ -2,23 +2,28 @@ import Actors from "./pages/Actors";
 import Directors from "./pages/Directors";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
    {
     path: "/",
-    element: <Home/>
+    element: <Home/>,
+    errorElement: <ErrorPage/>
    },
    {
     path: '/directors',
     element: <Directors/>,
+    errorElement: <ErrorPage/>
    },
    {
     path: '/actors',
-    element: <Actors/>
+    element: <Actors/>,
+    errorElement: <ErrorPage/>
    },
    {
     path: '/movie/:id',
-    element: <Movie/>
+    element: <Movie/>,
+    errorElement: <ErrorPage/>
    }
   ];
 
